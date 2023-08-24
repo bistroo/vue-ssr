73991e7c69eda654ebf7be41055c7856ed17941e
Youri van Mill <me@youri.dev>
2023-08-24 09:57:14 +0200

    feat: allow running non ssr in dev

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,13 @@
 import mri from 'mri'
 import { loadConfig } from 'c12'
-import { argv } from 'node:process'
+import { mergeConfig, createServer } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { argv, cwd } from 'node:process'
 import { VueSsrConfig } from './types'
 import { dev } from './node/dev'
 import { build } from './node/build'
 import { start } from './node/start'
+import { vueSsrPlugin } from './vue/plugin'
 
 const args = mri(argv.slice(2));
 const command = args._[0];
@@ -21,9 +24,24 @@ if (command === 'build') {
     host ?? config?.hostname ?? 'localhost'
   )
 } else {
-  await dev({
-    port: port ?? config?.port ?? 5173,
-    hostname: host ?? config?.hostname ?? 'localhost',
-    viteConfig: config?.vite,
-  })
+  if (ssr !== undefined ? /^true$/i.test(ssr) : config?.ssr ?? true) {
+    await dev({
+      port: port ?? config?.port ?? 5173,
+      hostname: host ?? config?.hostname ?? 'localhost',
+      viteConfig: config?.vite,
+    })
+  } else {
+    const server = await createServer(mergeConfig({
+      configFile: false,
+      base: '/',
+      root: cwd(),
+      plugins: [vue(), vueSsrPlugin()],
+      define: {
+        __SSR_APP__: false,
+      },
+    }, config?.vite ?? {}))
+    await server.listen()
+  
+    server.printUrls()
+  }
 }
